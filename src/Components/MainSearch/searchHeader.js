4c9ece36a6022562e162a68597e55165bcef136e
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-11-22 12:56:25 +0100

    multi query apis erach works for 2

--- src/Components/MainSearch/searchHeader.js
+++ src/Components/MainSearch/searchHeader.js
@@ -4,6 +4,12 @@ import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import SideBar from "./swipeableDrawer";
 import { CardActionArea, Typography } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import logo from "../../Assets/welcomeLogoBlue.JPG";
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -15,24 +21,42 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+},
 }));
 
 export default function SearchHeader() {
-  const classes = useStyles();
+  const classes = useStyles(); 
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3} style={{width:'85%', margin:'auto'}}>
         <Grid item xs={12}>
-          <Paper className={classes.paper}> <Typography variant="h2" gutterBottom>
-        Searching the MiddleSpace
-      </Typography></Paper>
+        <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={logo}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Search the Middle Space for Resources
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+          <SideBar/>
+             
+        </CardContent>
+      </CardActionArea>
         </Grid>
         <Grid item xs={12} sm={6}>
             <Paper>
          
-              <SideBar/>
-             
+           
               </Paper>
         </Grid>
         <Grid item xs={12} sm={6}>
